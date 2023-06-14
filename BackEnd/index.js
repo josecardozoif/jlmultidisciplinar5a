ba8a757def6133b5474d0c415771e1727d667a8b
josecardozoif <jose.araujo6@estudante.ifms.edu.br>
2023-06-14 15:19:27 -0400

    Instalação do Cors
    Adição do cors no BackEnd para funcionamento da aplicação.

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const Produto = require("./models/produto");
-
+const cors= require('cors');
 const app = express();
-
+app.use(cors());
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
